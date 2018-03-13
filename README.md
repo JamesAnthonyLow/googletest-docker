c2e3fcbcaa000e483543a9f02ce15fcd65562407
JamesAnthonyLow <jamesalowenthal@gmail.com>
2018-03-12 22:10:18 -0400

    wip

---
+++ README.md
@@ -0,0 +1,7 @@
+### Googletest Container
+
+```bash
+$ ./build.sh       # To build jamesanthonylow/googletest image
+$ source setenv.sh # To add googletest_<compile|make|exec> aliases to current session
+$ cd ./workspace && googletest_make <test name>_test
+```
--- setenv.sh
+++ setenv.sh
@@ -1,3 +1,3 @@
-alias googletest_compile="$PWD/docker_run.sh g++"
-alias googletest_make="$PWD/docker_run.sh make"
-alias googletest_exec="$PWD/docker_run.sh"
+alias googletest_compile="$PWD/googletest_run.sh g++"
+alias googletest_make="$PWD/googletest_run.sh make"
+alias googletest_exec="$PWD/googletest_run.sh"
